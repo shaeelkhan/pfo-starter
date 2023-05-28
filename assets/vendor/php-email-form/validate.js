ef089747022761637fc050338e5a220f250505e7
Shaeel786 <muhammad.shaeel@daoproptech.com>
2023-05-28 22:59:40 +0500

    Latest changes

--- assets/vendor/php-email-form/validate.js
+++ assets/vendor/php-email-form/validate.js
@@ -3,11 +3,27 @@
 * URL: https://bootstrapmade.com/php-email-form/
 * Author: BootstrapMade.com
 */
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.17.2/firebase-app.js";
+import { getFirestore, doc, setDoc, getDoc, getDocs, collection, addDoc, FieldValue } from "https://www.gstatic.com/firebasejs/9.17.2/firebase-firestore.js";
+import * as firebase from 'https://www.gstatic.com/firebasejs/9.17.2/firebase-app.js'
+const firebaseConfig = {
+  apiKey: "AIzaSyAuZV6URlfZG3YtY9AhNAZnz6Hi2t99PZg",
+  authDomain: "pfo-starter.firebaseapp.com",
+  projectId: "pfo-starter",
+  storageBucket: "pfo-starter.appspot.com",
+  messagingSenderId: "62430084522",
+  appId: "1:62430084522:web:74f57ff85a782076077fd3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
 (function () {
   "use strict";
 
   let forms = document.querySelectorAll('.php-email-form');
-
   forms.forEach(function (e) {
     e.addEventListener('submit', function (event) {
       event.preventDefault();
@@ -44,47 +60,33 @@
           displayError(thisForm, 'The reCaptcha javascript API url is not loaded!')
         }
       } else {
-        php_email_form_submit(thisForm, action, formData);
+        let payload = { name: thisForm[0].value, email: thisForm[1].value, subject: thisForm[2].value, message: thisForm[3].value }
+        php_email_form_submit(thisForm, action, formData, payload);
       }
     });
   });
 
-  function php_email_form_submit(thisForm, action, formData) {
-    fetch(action, {
-      method: 'POST',
-      body: formData,
-      headers: { 'X-Requested-With': 'XMLHttpRequest' }
-    })
-      .then(response => {
-        return response.text();
-      })
-      .then(data => {
-        thisForm.querySelector('.loading').classList.remove('d-block');
-        if (data.trim() == 'OK') {
-          thisForm.querySelector('.sent-message').classList.add('d-block');
-          thisForm.reset();
-        } else {
-          setTimeout(() => {
-            thisForm.querySelector('.loading').classList.remove('d-block');
-            thisForm.reset();
-            thisForm.querySelector('.sent-message').classList.add('d-block');
-            setTimeout(() => {
-              thisForm.querySelector('.sent-message').classList.remove('d-block');
-            }, 3000);
-          }, 1200);
-          thisForm.querySelector('.loading').classList.add('d-block');
 
-          // throw new Error(data ? data : 'Form submission failed and no error message returned from: ' + action); 
-        }
-      })
-      .catch((error) => {
-        displayError(thisForm, error);
-      });
+  function php_email_form_submit(thisForm, action, formData, payload) {
+    debugger
+    thisForm.querySelector('.loading').classList.add('d-block');
+    addDoc(collection(db, "messages"), { ...payload }).then((data) => {
+      console.log(data);
+      debugger
+      thisForm.querySelector('.loading').classList.remove('d-block');
+      thisForm.querySelector('.sent-message').classList.add('d-block');
+      thisForm.reset();
+      setTimeout(() => {
+        thisForm.querySelector('.sent-message').classList.remove('d-block');
+      }, 2000);
+    }).catch(err => {
+      displayError(thisForm, err);
+    });
   }
 
   function displayError(thisForm, error) {
     thisForm.querySelector('.loading').classList.remove('d-block');
-    thisForm.querySelector('.error-message').innerHTML = error;
+    thisForm.querySelector('.error-message').innerHTML = error.message;
     thisForm.querySelector('.error-message').classList.add('d-block');
   }
 
