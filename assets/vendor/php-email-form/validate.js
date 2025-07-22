3fd197b9c2c736c58fe2900f686c2c11839da569
muhammad-shaeel <m.shaeel@salla.sa>
2025-07-22 19:12:23 +0500

    Updated changes

--- assets/vendor/php-email-form/validate.js
+++ assets/vendor/php-email-form/validate.js
@@ -1,19 +1,23 @@
 /**
-* PHP Email Form Validation - v3.5
-* URL: https://bootstrapmade.com/php-email-form/
-* Author: BootstrapMade.com
-*/
+ * PHP Email Form Validation - v3.5
+ * URL: https://bootstrapmade.com/php-email-form/
+ * Author: BootstrapMade.com
+ */
 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.17.2/firebase-app.js";
-import { getFirestore, doc, setDoc, getDoc, getDocs, collection, addDoc, FieldValue } from "https://www.gstatic.com/firebasejs/9.17.2/firebase-firestore.js";
-import * as firebase from 'https://www.gstatic.com/firebasejs/9.17.2/firebase-app.js'
+import {
+  getFirestore,
+  collection,
+  addDoc,
+} from "https://www.gstatic.com/firebasejs/9.17.2/firebase-firestore.js";
+import * as firebase from "https://www.gstatic.com/firebasejs/9.17.2/firebase-app.js";
 const firebaseConfig = {
   apiKey: "AIzaSyAuZV6URlfZG3YtY9AhNAZnz6Hi2t99PZg",
   authDomain: "pfo-starter.firebaseapp.com",
   projectId: "pfo-starter",
   storageBucket: "pfo-starter.appspot.com",
   messagingSenderId: "62430084522",
-  appId: "1:62430084522:web:74f57ff85a782076077fd3"
+  appId: "1:62430084522:web:74f57ff85a782076077fd3",
 };
 
 // Initialize Firebase
@@ -23,23 +27,23 @@ const db = getFirestore(app);
 (function () {
   "use strict";
 
-  let forms = document.querySelectorAll('.php-email-form');
+  let forms = document.querySelectorAll(".php-email-form");
   forms.forEach(function (e) {
-    e.addEventListener('submit', function (event) {
+    e.addEventListener("submit", function (event) {
       event.preventDefault();
 
       let thisForm = this;
 
-      let action = thisForm.getAttribute('action');
-      let recaptcha = thisForm.getAttribute('data-recaptcha-site-key');
+      let action = thisForm.getAttribute("action");
+      let recaptcha = thisForm.getAttribute("data-recaptcha-site-key");
 
       if (!action) {
-        displayError(thisForm, 'The form action property is not set!')
+        displayError(thisForm, "The form action property is not set!");
         return;
       }
-      thisForm.querySelector('.loading').classList.add('d-block');
-      thisForm.querySelector('.error-message').classList.remove('d-block');
-      thisForm.querySelector('.sent-message').classList.remove('d-block');
+      thisForm.querySelector(".loading").classList.add("d-block");
+      thisForm.querySelector(".error-message").classList.remove("d-block");
+      thisForm.querySelector(".sent-message").classList.remove("d-block");
 
       let formData = new FormData(thisForm);
 
@@ -47,45 +51,55 @@ const db = getFirestore(app);
         if (typeof grecaptcha !== "undefined") {
           grecaptcha.ready(function () {
             try {
-              grecaptcha.execute(recaptcha, { action: 'php_email_form_submit' })
-                .then(token => {
-                  formData.set('recaptcha-response', token);
+              grecaptcha
+                .execute(recaptcha, { action: "php_email_form_submit" })
+                .then((token) => {
+                  formData.set("recaptcha-response", token);
                   php_email_form_submit(thisForm, action, formData);
-                })
+                });
             } catch (error) {
-              displayError(thisForm, error)
+              displayError(thisForm, error);
             }
           });
         } else {
-          displayError(thisForm, 'The reCaptcha javascript API url is not loaded!')
+          displayError(
+            thisForm,
+            "The reCaptcha javascript API url is not loaded!"
+          );
         }
       } else {
-        let payload = { name: thisForm[0].value, email: thisForm[1].value, subject: thisForm[2].value, message: thisForm[3].value }
+        let payload = {
+          name: thisForm[0].value,
+          email: thisForm[1].value,
+          subject: thisForm[2].value,
+          message: thisForm[3].value,
+          createdAt: new Date().toISOString(),
+        };
         php_email_form_submit(thisForm, action, formData, payload);
       }
     });
   });
 
-
   function php_email_form_submit(thisForm, action, formData, payload) {
-    thisForm.querySelector('.loading').classList.add('d-block');
-    addDoc(collection(db, "messages"), { ...payload }).then((data) => {
-      console.log(data);
-      thisForm.querySelector('.loading').classList.remove('d-block');
-      thisForm.querySelector('.sent-message').classList.add('d-block');
-      thisForm.reset();
-      setTimeout(() => {
-        thisForm.querySelector('.sent-message').classList.remove('d-block');
-      }, 2000);
-    }).catch(err => {
-      displayError(thisForm, err);
-    });
+    thisForm.querySelector(".loading").classList.add("d-block");
+    addDoc(collection(db, "messages"), { ...payload })
+      .then((data) => {
+        console.log(data);
+        thisForm.querySelector(".loading").classList.remove("d-block");
+        thisForm.querySelector(".sent-message").classList.add("d-block");
+        thisForm.reset();
+        setTimeout(() => {
+          thisForm.querySelector(".sent-message").classList.remove("d-block");
+        }, 2000);
+      })
+      .catch((err) => {
+        displayError(thisForm, err);
+      });
   }
 
   function displayError(thisForm, error) {
-    thisForm.querySelector('.loading').classList.remove('d-block');
-    thisForm.querySelector('.error-message').innerHTML = error.message;
-    thisForm.querySelector('.error-message').classList.add('d-block');
+    thisForm.querySelector(".loading").classList.remove("d-block");
+    thisForm.querySelector(".error-message").innerHTML = error.message;
+    thisForm.querySelector(".error-message").classList.add("d-block");
   }
-
 })();
